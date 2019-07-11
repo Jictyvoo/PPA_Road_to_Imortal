cc77f5610bd0494cbf1e69657f395a5c839e5373
Jictyvoo <jictyvoo.ecomp@gmail.com>
2019-07-11 12:57:26 -0300

    Added obfuscation compilation source code

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-cd src
-zip -r ../game.love ./*
-cd ../
-cat bin/love.exe game.love > bin/ppa_road_to_imortal.exe
+./bytecode-love.sh src/
+cat bin/love.exe PPARoadToImortal-x86_64.love > bin/ppa_road_to_imortal.exe
 
 cd bin/
 zip -r ../ppa_road_to_imortal.zip SDL2.dll OpenAL32.dll ppa_road_to_imortal.exe license.txt love.dll lua51.dll mpg123.dll msvcp120.dll msvcr120.dll
